1649f286aacfb84b52d6cdc4a812e51c9421554e
Alex Ling <hkalexling@gmail.com>
2020-02-20 23:39:54 +0000

    Split routes in `server.cr` into small files

---
+++ src/routes/main.cr
@@ -0,0 +1,56 @@
+require "./router"
+
+class MainRouter < Router
+	def setup
+		get "/login" do |env|
+			render "src/views/login.ecr"
+		end
+
+		get "/logout" do |env|
+			begin
+				cookie = env.request.cookies
+					.find { |c| c.name == "token" }.not_nil!
+				@context.storage.logout cookie.value
+			rescue e
+				@context.error "Error when attempting to log out: #{e}"
+			ensure
+				env.redirect "/login"
+			end
+		end
+
+		post "/login" do |env|
+			begin
+				username = env.params.body["username"]
+				password = env.params.body["password"]
+				token = @context.storage.verify_user(username, password)
+					.not_nil!
+
+				cookie = HTTP::Cookie.new "token", token
+				env.response.cookies << cookie
+				env.redirect "/"
+			rescue
+				env.redirect "/login"
+			end
+		end
+		get "/" do |env|
+			titles = @context.library.titles
+			username = get_username env
+			percentage = titles.map &.load_percetage username
+			layout "index"
+		end
+
+		get "/book/:title" do |env|
+			begin
+				title = (@context.library.get_title env.params.url["title"])
+					.not_nil!
+				username = get_username env
+				percentage = title.entries.map { |e|
+					title.load_percetage username, e.title }
+				layout "title"
+			rescue e
+				@context.error e
+				env.response.status_code = 404
+			end
+		end
+	end
+end
