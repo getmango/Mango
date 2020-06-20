5f21653e070cb1be84a87f458cf6fdc5b66bc523
Alex Ling <hkalexling@gmail.com>
2020-06-20 16:25:14 +0000

    Bump version to v0.7.1

--- src/mango.cr
+++ src/mango.cr
@@ -4,7 +4,7 @@ require "./mangadex/*"
 require "option_parser"
 require "clim"
 
-MANGO_VERSION = "0.7.0"
+MANGO_VERSION = "0.7.1"
 
 macro common_option
   option "-c PATH", "--config=PATH", type: String,
