5027a911cd56912632e38964ac933d16fdb1adb7
Alex Ling <hkalexling@gmail.com>
2020-08-06 17:01:53 +0000

    Respect the *_PROXY environment variables (#94)

---
+++ src/util/proxy.cr
@@ -0,0 +1,42 @@
+require "http_proxy"
+
+# Monkey-patch `HTTP::Client` to make it respect the `*_PROXY`
+#   environment variables
+module HTTP
+  class Client
+    private def self.exec(uri : URI, tls : TLSContext = nil)
+      Logger.debug "Using monkey-patched HTTP::Client"
+      previous_def uri, tls do |client, path|
+        client.set_proxy get_proxy uri
+        yield client, path
+      end
+    end
+  end
+end
+
+private def get_proxy(uri : URI) : HTTP::Proxy::Client?
+  no_proxy = ENV["no_proxy"]? || ENV["NO_PROXY"]?
+  return if no_proxy &&
+            no_proxy.split(",").any? &.== uri.hostname
+
+  case uri.scheme
+  when "http"
+    env_to_proxy "http_proxy"
+  when "https"
+    env_to_proxy "https_proxy"
+  else
+    nil
+  end
+end
+
+private def env_to_proxy(key : String) : HTTP::Proxy::Client?
+  val = ENV[key.downcase]? || ENV[key.upcase]?
+  return if val.nil?
+
+  begin
+    uri = URI.parse val
+    HTTP::Proxy::Client.new uri.hostname.not_nil!, uri.port.not_nil!
+  rescue
+    nil
+  end
+end
