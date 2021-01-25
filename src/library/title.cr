c51cb28df210cef6444fb9d11927e251b504ef33
Leeingnyo <dyd1928@naver.com>
2021-01-25 23:13:35 +0900

    make filename extension downcase for comparing

--- src/library/title.cr
+++ src/library/title.cr
@@ -36,7 +36,7 @@ class Title
         @title_ids << title.id
         next
       end
-      if [".zip", ".cbz", ".rar", ".cbr"].includes? File.extname path
+      if [".zip", ".cbz", ".rar", ".cbr"].includes? (File.extname path).downcase
         entry = Entry.new path, self
         @entries << entry if entry.pages > 0 || entry.err_msg
       end
