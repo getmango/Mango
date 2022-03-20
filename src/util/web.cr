c3736d222c9976d7988888bbbf4e90f4c8af1752
Alex Ling <hkalexling@gmail.com>
2022-03-20 10:01:44 +0000

    Fix long line

--- src/util/web.cr
+++ src/util/web.cr
@@ -60,7 +60,9 @@ end
 
 macro cors
   env.response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
-  env.response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept, Authorization"
+  env.response.headers["Access-Control-Allow-Headers"] = "X-Requested-With," \
+    "X-HTTP-Method-Override, Content-Type, Cache-Control, Accept," \
+    "Authorization"
   env.response.headers["Access-Control-Allow-Origin"] = "*"
 end
 
